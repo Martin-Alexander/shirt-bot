1ee7c24dfccdbb51c4ce318056f2339470d40a07
Dimitri Bosch <dimitribosch@hotmail.com>
2019-06-26 16:08:28 +0200

    Test message slack

--- app/services/send_message_service.rb
+++ app/services/send_message_service.rb
@@ -1,9 +1,31 @@
 class SendMessageService
-  def initialize(channel_id)
+  attr_reader :channel_id
+
+  def initialize(channel_id = 'DKWP42RRN')
     @channel_id = channel_id
   end
 
   def send
-
+    SlackClient.chat_postMessage(
+      channel: 'CKWNMQ3NZ',
+      text: 'Do you want a Le Wagon t-shirt',
+      attachments: [
+        {
+          text: 'We need to get some information first',
+          fallback: 'You are unable to choose a game',
+          callback_id: 'wopr_game',
+          color: '#3AA3E3',
+          attachment_type: 'default',
+          actions: [
+            {
+              name: 'sizer',
+              text: 'Choose my t-shirt',
+              type: 'button',
+              value: 'choose'
+            }
+          ]
+        }
+      ]
+    )
   end
 end
