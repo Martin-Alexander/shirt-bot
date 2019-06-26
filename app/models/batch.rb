c9ecbe72bda9385359b04274e8f400dce21302b2
Dimitri Bosch <dimitribosch@hotmail.com>
2019-06-26 18:57:27 +0200

    Callback

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -3,4 +3,8 @@ class Batch < ApplicationRecord
 
   validates :number, presence: true, uniqueness: true
   validates :issuer_slack_user_id, presence: true
+
+  def send_csv
+    CsvResults.send(batch_number: number)
+  end
 end
--- app/models/shirt.rb
+++ app/models/shirt.rb
@@ -2,4 +2,10 @@ class Shirt < ApplicationRecord
   belongs_to :batch
 
   validates :slack_user_id, uniqueness: true, presence: true
+
+  after_update :check_batch_full
+
+  def check_batch_full
+    batch.send_csv if batch.shirts.size == batch.shirts.where.not(size: nil).size
+  end
 end
